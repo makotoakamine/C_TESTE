e4a31052f5f1f352a1228ee7045c23c45faae4ba
makotoakamine <makotoakamine12@gmail.com>
2015-06-30 16:35:15 +0000

    Commit teste do branc1

--- hello-c-world.c
+++ hello-c-world.c
@@ -13,14 +13,17 @@ int main() {
         
     for(i=0;i<max;i++)
         perfectCount[i % 10]++;
-            
+    printf("Contagem distribuida----------\n");            
     for(i=0;i<10;i++)
         printf(" [%d] = %d \n",i+1,perfectCount[i]);
-    printf("---\n");
+    printf("Contagem aleatória------------\n");            
     for(i=0;i<10;i++)
         printf(" [%d] = %d \n",i+1,count[i]);
         
     printf("------------------------\n");
+    printf("\n");
+    printf("\n");
+    printf("Prob aleatória------------\n");            
     for(i=0;i<10;i++)
         printf(" [%d] = %f \n",i+1,(float)perfectCount[i]/(float)max);
     printf("---\n");
@@ -28,10 +31,13 @@ int main() {
         printf(" [%d] = %f \n",i+1,(float)count[i]/(float)max);
     printf("------------------------\n");
 
+
     for(i=0;i<10;i++)
         printf(" [%d] = %d \n",i+1,perfectCount[i] - count[i]);
     printf("Desvio---------\n");
     for(i=0;i<10;i++)
         printf(" [%i] = %f \n",i+1,1-((float)perfectCount[i]/(float)count[i]));
     return 0;
+    
+    
 }
